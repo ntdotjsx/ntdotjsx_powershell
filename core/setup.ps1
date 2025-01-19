c9d686d4dfa911f0f461dd60bc1c9206424d3d46
Devy <91049342+ntdotjsx@users.noreply.github.com>
2025-01-20 05:12:17 +0700

    Update setup.ps1

--- core/setup.ps1
+++ core/setup.ps1
@@ -1,6 +1,10 @@
 if (-not (Get-Command oh-my-posh -ErrorAction SilentlyContinue)) {
     Write-Host "oh-my-posh is not installed. Installing..."
-    Install-Module -Name oh-my-posh -Force -Scope CurrentUser
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+    $env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
+    (Get-Command oh-my-posh).Source
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+    oh-my-posh font install
     Write-Host "oh-my-posh installed successfully."
 } else {
     Write-Host "oh-my-posh is already installed."
@@ -16,6 +20,7 @@ if (-not (Get-Module -ListAvailable PSReadLine)) {
 
 if (-not (Test-Path -Path $PROFILE)) {
     Write-Host "$PROFILE does not exist. Creating..."
+    oh-my-posh get shell
     New-Item -Path $PROFILE -Type File -Force
     Write-Host "$PROFILE created successfully."
 }
