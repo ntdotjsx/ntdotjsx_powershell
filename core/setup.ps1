55ac72eab1acef698c00b84a10770764ee587597
Devy <91049342+ntdotjsx@users.noreply.github.com>
2025-01-20 03:45:09 +0700

    new version

---
+++ core/setup.ps1
@@ -0,0 +1,22 @@
+# ตรวจสอบว่า oh-my-posh ติดตั้งแล้วหรือยัง
+if (-not (Get-Command oh-my-posh -ErrorAction SilentlyContinue)) {
+    Write-Host "oh-my-posh is not installed. Installing..."
+    Install-Module -Name oh-my-posh -Force -Scope CurrentUser
+    Write-Host "oh-my-posh installed successfully."
+} else {
+    Write-Host "oh-my-posh is already installed."
+}
+
+if (-not (Get-Module -ListAvailable PSReadLine)) {
+    Write-Host "PSReadLine is not installed. Installing..."
+    Install-Module -Name PSReadLine -Force -Scope CurrentUser
+    Write-Host "PSReadLine installed successfully."
+} else {
+    Write-Host "PSReadLine is already installed."
+}
+
+if (-not (Test-Path -Path $PROFILE)) {
+    Write-Host "$PROFILE does not exist. Creating..."
+    New-Item -Path $PROFILE -Type File -Force
+    Write-Host "$PROFILE created successfully."
+}
--- setup.bat
+++ setup.bat
@@ -2,54 +2,17 @@
 Mode 120,35
 title ntdotjsx setting
 Reg.exe add HKLM /F >nul 2>&1
-for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a" & set "COL=%%b")
 
 :lobby
 sleep 800
 chcp 65001
 cls
-set "emojis=🔍 🗼 👀 🔎"
-set "filename=ntdotjsx.reg"
-set "fileFound=false"
-
 pause
 cls
 echo Please wait...
 
-for %%d in (C D E) do (
-    for /L %%i in (1,1,5) do (
-        for %%e in (%emojis%) do (
-            cls
-            echo %%e Downloading ...
-            timeout /t 1 >nul
-        )
-    )
-    
+regedit /s "./core/ntdotjsx.reg"
+powershell -ExecutionPolicy Bypass -File "./core/setup.ps1"
+powershell -Command "& './core/profile.ps1'"
 
-    for /f "delims=" %%f in ('dir %%d:\%filename% /s /b 2^>nul') do (
-        if exist "%%f" (
-            cls
-            echo File found: %%f
-            choice /C YN /M "Press Y to confirm, N to cancel"
-            if errorlevel 2 (
-                cls
-                echo Installation canceled.
-                pause
-                goto :EOF
-            )
-            if errorlevel 1 (
-                regedit /s "%%f"
-                cls
-                echo File has been successfully imported into the registry.
-                powershell -Command "& './core/profile.ps1'"
-                @REM powershell -Command "notepad $PROFILE"
-                pause
-                goto :EOF
-            )
-        )
-    )
-)
-
-cls
-echo File "%filename%" was not found on any drive.
 pause
